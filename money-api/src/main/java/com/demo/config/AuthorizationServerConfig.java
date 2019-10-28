9a78ac48ea8a907c90e0527c2a93c0c07403d247
marco <lelis685@gmail.com>
2019-10-28 18:57:59 -0300

    config jwt

--- money-api/src/main/java/com/demo/config/AuthorizationServerConfig.java
+++ money-api/src/main/java/com/demo/config/AuthorizationServerConfig.java
@@ -9,7 +9,8 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 @EnableAuthorizationServer
@@ -22,8 +23,8 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
 		clients.inMemory()
-		.withClient("angular")
-		.secret("angular")
+		.withClient("alga")
+		.secret("alga")
 		.scopes("read","write")
 		.authorizedGrantTypes("password")
 		.accessTokenValiditySeconds(1800);
@@ -34,13 +35,22 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
 		endpoints
 		.tokenStore(tokenStore())
+		.accessTokenConverter(accessTokenConverter())
 		.authenticationManager(authenticationManager);
 	}
 
 
 	@Bean
+	public JwtAccessTokenConverter accessTokenConverter() {
+		JwtAccessTokenConverter accessTokenConverter = new JwtAccessTokenConverter();
+		accessTokenConverter.setSigningKey("alga");
+		return accessTokenConverter;
+	}
+
+
+	@Bean
 	public TokenStore tokenStore() {
-		return new InMemoryTokenStore();
+		return new JwtTokenStore(accessTokenConverter());
 	}
 	
 
